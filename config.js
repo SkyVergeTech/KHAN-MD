1cc5f691cabcc4725102083aaa787e8c3c446310
SkyVergeTech <skyverge-tech@outlook.com>
2025-09-08 01:22:53 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "IK~ZbFG1S4J#aOxHLK-fEO6LTg4Z1YG5ALVSTMKu2vTjD7nDesSzfvQ",
 // add your Session Id
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
